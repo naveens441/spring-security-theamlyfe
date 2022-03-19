b3996ed2ccf4a240a90a24889c482a238c8d84fb
Naveen_Kumar_Singh <naveen_kumar_singh@epam.com>
2022-03-19 11:48:03 +0530

    Initial commit

---
+++ src/main/java/neo/springsecuritytheamlyfe/SpringSecurityTheamlyfeApplication.java
@@ -0,0 +1,13 @@
+package neo.springsecuritytheamlyfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTheamlyfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTheamlyfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neo/springsecuritytheamlyfe/SpringSecurityTheamlyfeApplicationTests.java
@@ -0,0 +1,13 @@
+package neo.springsecuritytheamlyfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTheamlyfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
